c7f0400107c70c5259790ba17d80faafb27ce6b2
PrasanthChettri <prasanthchettri3@gmail.com>
2021-04-06 11:50:29 +0530

    simple calculator going on

--- a.hs
+++ a.hs
@@ -1,9 +1,23 @@
-import System.IO
+type Operator = Double -> Double -> Double
+type Entry = (String, Operator)
+type Register = [Entry]
 
-main :: IO()
-main = do
-    putStr " " 
-    hFlush stdout
-    line <- getLine
-    print line
-    main
+operatorRegister :: Register
+operatorRegister = [
+                ("+", (+)),
+                ("*", (*))
+            ]
+            
+main = print $ calculate "2 * 5 + 5"
+calculate :: String -> Double
+calculate = eval operatorRegister . words
+            
+eval :: Register -> [String] -> Double
+eval _ [number] = read number
+eval ((operator, function):rest) unparsed =
+    case span (/=operator) unparsed of
+        (_, []) -> eval rest unparsed
+        (beforeOperator, afterOperator) -> 
+            function
+                (eval operatorRegister beforeOperator)
+                (eval operatorRegister $ drop 1 afterOperator)
